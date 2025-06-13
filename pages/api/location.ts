36a6a35a5fde25bcc1283ffb451db3e5ac9712cc
herwork06 <herwork06@gmail.com>
2025-06-13 23:11:35 +0000

    Configure API to use Edge Runtime functions

--- pages/api/location.ts
+++ pages/api/location.ts
@@ -1,9 +1,14 @@
+import { headers } from "next/headers";
+
 /* eslint-disable @typescript-eslint/no-explicit-any */
-export default function handler(req: { headers: { [x: string]: any; }; }, res: { status: (arg0: number) => { (): any; new(): any; json: { (arg0: { city: any; country: any; lat: any; lon: any; }): void; new(): any; }; }; }) {
-  const city = req.headers["cf-city"];
-  const country = req.headers["cf-ipcountry"];
-  const lat = req.headers["cf-latitude"];
-  const lon = req.headers["cf-longitude"];
-  console.log({ city, country, lat, lon })
-  res.status(200).json({ city, country, lat, lon });
+export default async function handler(req: { headers: { [x: string]: any; }; }, res: { status: (arg0: number) => { (): any; new(): any; json: { (arg0: { city: any; country: any; lat: any; lon: any; }): void; new(): any; }; }; }) {
+
+    const headersList = await headers()
+    const city = headersList.get("cf-city")
+    const country = headersList.get("cf-ipcountry");
+    const lat = headersList.get("cf-latitude");
+    const lon = headersList.get("cf-longitude");
+    console.log({ city, country, lat, lon })
+    res.status(200).json({ city, country, lat, lon });
+
 }
