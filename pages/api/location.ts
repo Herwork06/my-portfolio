03479d86ebde4bc1cf3aeb3475f2a1551a4e4c93
herwork06 <herwork06@gmail.com>
2025-06-13 23:25:06 +0000

    Redoing response and added correct types

--- pages/api/location.ts
+++ pages/api/location.ts
@@ -1,15 +1,25 @@
 export const runtime = 'edge';
 import { headers } from "next/headers";
+import type { NextApiRequest, NextApiResponse } from 'next'
 
-/* eslint-disable @typescript-eslint/no-explicit-any */
-export default async function handler(req: { headers: { [x: string]: any; }; }, res: { status: (arg0: number) => { (): any; new(): any; json: { (arg0: { city: any; country: any; lat: any; lon: any; }): void; new(): any; }; }; }) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+    try {
+        const headersList = await headers()
+        const city = headersList.get("cf-city")
+        const country = headersList.get("cf-ipcountry");
+        const lat = headersList.get("cf-latitude");
+        const long = headersList.get("cf-longitude");
+        console.log({ city, country, lat, long })
+        res.status(200).json({ city, country, lat, long });
+    } catch(err) {
+        res.status(500).json({ error: {
+            message: 'failed to retrive data',
+            errorMessage: err
+        }})
+    }
 
-    const headersList = await headers()
-    const city = headersList.get("cf-city")
-    const country = headersList.get("cf-ipcountry");
-    const lat = headersList.get("cf-latitude");
-    const lon = headersList.get("cf-longitude");
-    console.log({ city, country, lat, lon })
-    res.status(200).json({ city, country, lat, lon });
 
 }
